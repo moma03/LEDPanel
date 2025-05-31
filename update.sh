eff0d47e6f6b4cbf364e19f4607168eac4cc2a74
Moritz Manegold <mmanegold@web.de>
2025-05-31 22:05:08 +0200

    Fix script

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # This script updates the rpi-rgb-led-matrix submodule in the current repository.
-git subtree pull --prefix=rpi-rgb-led-matrix --squash origin master
+git subtree pull --prefix=rpi-rgb-led-matrix https://github.com/hzeller/rpi-rgb-led-matrix.git master --squash
